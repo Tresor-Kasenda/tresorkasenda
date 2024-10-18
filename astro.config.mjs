0af9b60957051c95182cf382296304bd23de2791
tresor kasenda <tresorkasendat@gmail.com>
2024-10-18 22:44:24 +0200

    feat (header): review header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import service from 'astro/assets/services/squoosh';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://tresorkasenda.vercel.app",
+  prefetch: {
+    prefetchAll: true
+  }
 });

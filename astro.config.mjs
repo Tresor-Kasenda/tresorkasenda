a4127297f84157901271ce758a6c465623af7113
tresor kasenda <tresorkasendat@gmail.com>
2024-10-28 21:49:53 +0200

    change design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import service from 'astro/assets/services/squoosh';
